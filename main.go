0ff26f4cdc9e17a8eef35fbcb73e586d223bf36c
Worawut2547 <wutsab@gmail.com>
2025-07-02 10:23:12 +0700

    CRUD -> U

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 	app.Get("/books", getBooks)
 	app.Get("/book/:id",getBook)
 	app.Post("/book" ,createBook)
+	app.Put("/book/:id",updateBook)
 
 	fmt.Printf("Server is Running on Ports 6000\n")
 	app.Listen(":6000")
@@ -60,4 +61,27 @@ func createBook(c *fiber.Ctx) error {
 
 	books = append(books , *book)
 	return c.JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+	id , err := strconv.Atoi(c.Params("id"))
+
+	if err != nil{
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	bookUpdate := new(Book)
+	if err := c.BodyParser(bookUpdate); err != nil{
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i,book := range books{
+		if book.ID == id{
+			book.Title = bookUpdate.Title
+			book.Author = bookUpdate.Author
+			books[i] = book
+			return c.JSON(book)
+		}
+	}
+	return c.SendStatus(fiber.StatusNotFound)
 }
